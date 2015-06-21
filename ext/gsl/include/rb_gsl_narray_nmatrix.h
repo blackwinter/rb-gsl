da450468b3fb6cb304ddd2f52f6da01df0abd1bb
Daniel Mendler <mail@daniel-mendler.de>
2015-06-21 14:57:42 +0200

    import gsl-nmatrix code

--- ext/gsl/include/rb_gsl_narray_nmatrix.h
+++ ext/gsl/include/rb_gsl_narray_nmatrix.h
@@ -28,4 +28,35 @@ gsl_vector_complex* na_to_gv_complex(VALUE na);
 gsl_vector_complex_view* na_to_gv_complex_view(VALUE na);
 
 #endif // HAVE_NARRAY_H
+
+#ifdef HAVE_NMATRIX_H
+
+#include "nmatrix.h"
+
+// in array.c
+gsl_vector* make_cvector_from_nvector(VALUE vec);
+void cvector_set_from_nvector(gsl_vector*, VALUE);
+void carray_set_from_nvector(double*, VALUE);
+
+// in gsl_nmatrix.c
+VALUE rb_gsl_nm_to_gsl_matrix(VALUE obj, VALUE nm);
+VALUE rb_gsl_nv_to_gsl_vector(VALUE obj, VALUE nv);
+gsl_matrix* nm_to_gm(VALUE nm);
+gsl_vector* nv_to_gv(VALUE nv);
+
+VALUE rb_gsl_nm_to_gsl_matrix_complex(VALUE obj, VALUE nm);
+VALUE rb_gsl_nv_to_gsl_vector_complex(VALUE obj, VALUE nv);
+gsl_matrix_complex* nm_to_gm_complex(VALUE nm);
+gsl_vector_complex* nv_to_gv_complex(VALUE nv);
+
+VALUE rb_gsl_nm_to_gsl_matrix_int(VALUE obj, VALUE nm);
+VALUE rb_gsl_nv_to_gsl_vector_int(VALUE obj, VALUE nv);
+gsl_matrix_int* nm_to_gm_int(VALUE nm);
+gsl_vector_int* nv_to_gv_int(VALUE nv);
+
+// from NMatrix:
+extern VALUE cNVector, CNMatrix;
+
+#endif // HAVE_NMATRIX_H
+
 #endif // RB_GSL_WITH_NARRAY_H
